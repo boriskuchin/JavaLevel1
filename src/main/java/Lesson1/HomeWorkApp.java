e6627b0f77d6c5ebefd633bf4e35b700ad90a9cc
Boris Kuchin <bvkuchin@gmail.com>
2022-05-14 10:09:02 +0300

    part 1,2,3,4 of homework1 solved

--- src/main/java/Lesson1/HomeWorkApp.java
+++ src/main/java/Lesson1/HomeWorkApp.java
@@ -1,11 +1,62 @@
 package Lesson1;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class HomeWorkApp {
 
 
     public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+
+    }
+
+    public static void printThreeWords() {
+        System.out.println("----------printThreeWords()----------");
+        System.out.println("Orange\n" + "Banana\n" + "Apple");
+    }
+
+    public static void checkSumSign() {
+        int varA = ThreadLocalRandom.current().nextInt(10) - 5;
+        int varB = ThreadLocalRandom.current().nextInt(10) - 5;
+
+        System.out.println("----------checkSumSign()----------");
+        System.out.println("VarA = " + varA + ", " + "VarA = " + varB);
+        if ((varA + varB) >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
 
 
     }
+
+    public static void printColor() {
+        int value = (ThreadLocalRandom.current().nextInt(400) - 200);
+
+        System.out.println("----------printColor()----------");
+        System.out.println("value = " + value);
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if ((value > 0) && (value <= 100)) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+
+        }
+    }
+
+    private static void compareNumbers() {
+        int varA = ThreadLocalRandom.current().nextInt(10) - 5;
+        int varB = ThreadLocalRandom.current().nextInt(10) - 5;
+
+        System.out.println("----------compareNumbers()----------");
+        System.out.println("VarA = " + varA + ", " + "VarA = " + varB);
+    }
+
+
 }
 
