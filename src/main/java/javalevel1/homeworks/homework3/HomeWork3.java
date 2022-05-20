1cb70fe4520cf511adae7bbfc10abd8b45182f19
Boris Kuchin <bvkuchin@gmail.com>
2022-05-20 10:59:40 +0300

    homework3 2 task done

--- src/main/java/javalevel1/homeworks/homework3/HomeWork3.java
+++ src/main/java/javalevel1/homeworks/homework3/HomeWork3.java
@@ -9,10 +9,13 @@ public class HomeWork3 {
 
     public static void main(String[] args) {
         createBinaryArray();
+        fillArray();
     }
 
     public static void createBinaryArray() {
         /*1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;*/
+
+        System.out.println("---------------Задание 1---------------");
 
         // Создаем массив случайной длины и заполняем его слуяайными значениями
         int randomBoundary = 15;
@@ -22,7 +25,6 @@ public class HomeWork3 {
         }
         System.out.printf("Изходный массив:   %s %n",Arrays.toString(array));
 
-
         //Меняем значения
         for (int i = 0; i < array.length; i++) {
             array[i] = 1 - array[i];
@@ -30,7 +32,20 @@ public class HomeWork3 {
 
         System.out.printf("Измененный массив: %s %n",Arrays.toString(array));
 
+
     }
 
+    public static void fillArray() {
+        /*2. Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100;*/
 
+        System.out.println("---------------Задание 2---------------");
+
+        int[] array = new int[100];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+
+        System.out.printf("Результат: %s%n", Arrays.toString(array));
+
+    }
 }
