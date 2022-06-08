650bedb86c5af9e634c9aaae2f95415e4c2b7887
Boris Kuchin <bvkuchin@gmail.com>
2022-06-09 00:17:47 +0300

    homework_8 renamed button variables

--- src/main/java/javalevel1/homeworks/homework8/CalculatorApplication.java
+++ src/main/java/javalevel1/homeworks/homework8/CalculatorApplication.java
@@ -65,20 +65,20 @@ public class CalculatorApplication extends JFrame {
     private JPanel createOperatorPanel() {
         JPanel operatorPanel = new JPanel();
         operatorPanel.setLayout(new GridLayout(4, 1));
-        JButton plusBotton = new MyJButton("+");
-        JButton minusBotton = new MyJButton("-");
-        JButton myltiplyBotton = new MyJButton("*");
-        JButton devisionBotton = new MyJButton("/");
+        JButton plusButton = new MyJButton("+");
+        JButton minusButton = new MyJButton("-");
+        JButton multiplyButton = new MyJButton("*");
+        JButton devideButton = new MyJButton("/");
 
-        plusBotton.addActionListener(new OperatorButtonListener(this));
-        minusBotton.addActionListener(new OperatorButtonListener(this));
-        myltiplyBotton.addActionListener(new OperatorButtonListener(this));
-        devisionBotton.addActionListener(new OperatorButtonListener(this));
+        plusButton.addActionListener(new OperatorButtonListener(this));
+        minusButton.addActionListener(new OperatorButtonListener(this));
+        multiplyButton.addActionListener(new OperatorButtonListener(this));
+        devideButton.addActionListener(new OperatorButtonListener(this));
 
-        operatorPanel.add(plusBotton);
-        operatorPanel.add(minusBotton);
-        operatorPanel.add(myltiplyBotton);
-        operatorPanel.add(devisionBotton);
+        operatorPanel.add(plusButton);
+        operatorPanel.add(minusButton);
+        operatorPanel.add(multiplyButton);
+        operatorPanel.add(devideButton);
 
         return operatorPanel;
     }
