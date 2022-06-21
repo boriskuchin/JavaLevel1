98f49bb7778a7fbc7908b76a6de938e6e76003ee
Boris Kuchin <bvkuchin@gmail.com>
2022-06-21 14:50:04 +0300

    homework_2 level_2. done

---
+++ src/main/java/javalevel2/homeworks/homework2/exceptionClasses/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package javalevel2.homeworks.homework2.exceptionClasses;
+
+public class MyArrayDataException extends NumberFormatException {
+    public MyArrayDataException(String s) {
+        super(s);
+    }
+}
---
+++ src/main/java/javalevel2/homeworks/homework2/exceptionClasses/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package javalevel2.homeworks.homework2.exceptionClasses;
+
+public class MyArraySizeException extends Exception {
+    public MyArraySizeException(String s) {
+        super(s);
+    }
+}
